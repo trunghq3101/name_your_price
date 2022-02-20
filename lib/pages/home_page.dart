cac976fc63a99bec2216635b4d436d6dd6d706aa
trunghq3101 <trunghq3101@gmail.com>
2022-02-20 15:05:21 +0000

    Check result & show next product

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class ProductInfo {
+  final String name;
+  final int price;
+
+  ProductInfo(this.name, this.price);
+}
+
+final products = [
+  ProductInfo('Wireless mouse', 3),
+  ProductInfo('Keyboard', 5),
+  ProductInfo('Camera', 8),
+  ProductInfo('Speaker', 4),
+  ProductInfo('iPad', 1000),
+];
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentProductIndex = 0;
+  int? _inputtedPrice;
+  String _result = '';
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +35,52 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             const Padding(padding: EdgeInsets.only(top: 80)),
-            const Text('Bluetooth mouse'),
+            Text(products[_currentProductIndex].name),
             const Padding(padding: EdgeInsets.only(top: 40)),
             SizedBox(
               width: 200,
               child: TextField(
+                key: const Key('priceInput'),
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                onChanged: (value) {
+                  _inputtedPrice = int.tryParse(value);
+                },
               ),
             ),
             const Padding(padding: EdgeInsets.only(top: 40)),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _result =
+                      _inputtedPrice == products[_currentProductIndex].price
+                          ? 'pass'
+                          : 'fail';
+                });
+              },
               child: const Text('Check'),
-            )
+            ),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: Text(
+                _result,
+                key: const Key('result'),
+              ),
+            ),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _result = '';
+                    if (_currentProductIndex < 4) {
+                      _currentProductIndex++;
+                    }
+                  });
+                },
+                child: const Text('Next'),
+              ),
+            ),
           ],
         ),
       ),
